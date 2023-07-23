c4ef6336759bf24a7f5484f980b816652ff77158
Bar-Barina <itsbarbarina@gmail.com>
2023-07-23 16:26:40 +0300

    added drawer

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from "expo-status-bar";
-import {
- Button,
-} from "react-native";
 import CategoryIndex from "./views/CategoryIndex";
 import MealsIndex from "./views/MealsIndex";
 import MealDetails from "./views/MealDetails";
+import Favorites from "./views/Favorites"
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator()
+
+function DrawerNavigator() {
+  return <Drawer.Navigator>
+    <Drawer.Screen name="Categories" component={CategoryIndex}/>
+    <Drawer.Screen name="Favorites"  component={Favorites}/>
+  </Drawer.Navigator>
+}
+
 
 export default function App() {
   return (
@@ -22,7 +30,9 @@ export default function App() {
             contentStyle: { backgroundColor: "#C4A484" },
           }}
         >
-          <Stack.Screen name="All Categories" component={CategoryIndex} />
+          <Stack.Screen name="All Categories" component={DrawerNavigator} />
+
+
           <Stack.Screen name="Meals List" component={MealsIndex} />
           <Stack.Screen name="Meal Details" component={MealDetails} />
         </Stack.Navigator>
