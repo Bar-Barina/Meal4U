a597e18315951254372116d63fbbf446d72aadb1
Bar-Barina <itsbarbarina@gmail.com>
2023-07-23 23:57:30 +0300

    added logo

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,30 +1,40 @@
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
 import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
 
-export default function Home({navigation}) {
-
-function getStarted() {
-navigation.navigate("Categories")
-}
+export default function Home({ navigation }) {
+  function getStarted() {
+    navigation.navigate("Categories");
+  }
 
   return (
-    <LinearGradient colors={["#f7f7ff", "#414360"]}  style={styles.homeContainer}>
-      <Text style={styles.homeText}>Meal4U</Text>
-      <Button title="Get Started!" onPress={getStarted}  android_ripple={{ color: "#CCC7" }} />
-    </LinearGradient >
+    <LinearGradient
+      colors={["#414360", "#f7f7ff", "#aa4203"]}
+      style={styles.homeContainer}
+    >
+      <Image
+        source={require("../assets/images/logo.png")}
+        style={styles.image}
+      />
+      <Button
+        title="Get Started!"
+        onPress={getStarted}
+        android_ripple={{ color: "#CCC7" }}
+      />
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   homeContainer: {
-    flex:1,
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
     elevation: 4,
   },
-  homeText: {
-    color:"#242526",
-    fontSize: 90,
+  image: {
+    margin: 50,
+    width: 230,
+    height: 260,
   },
 });
