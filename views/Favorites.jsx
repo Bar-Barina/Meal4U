8c4bdf3150ca72a90111c2a43eef7c735bbcb074
Bar-Barina <itsbarbarina@gmail.com>
2023-07-24 20:29:12 +0300

    finished for now.

--- views/Favorites.jsx
+++ views/Favorites.jsx
@@ -1,12 +1,14 @@
 import { Text } from "react-native";
 import { useSelector } from "react-redux";
-import  MealList  from "../cmps/Meal/MealList";
+import MealList from "../cmps/Meal/MealList";
 import { MEALS } from "../data/dummy-data";
 
 export default function Favorites() {
   const favoriteMealsIds = useSelector((state) => state.favorite.ids);
 
-  const favoriteMeals = MEALS.filter((meal) =>favoriteMealsIds.includes(meal.id));
+  const favoriteMeals = MEALS.filter((meal) =>
+    favoriteMealsIds.includes(meal.id)
+  );
 
   if (!favoriteMeals || favoriteMeals.length === 0)
     return <Text>No favorite meals for now...</Text>;
